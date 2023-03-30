216f67399606f2f5ab4daf26963ee06ca6a6ebab
Aliaamahm0ud <loloeltorky165@gmail.com>
2023-03-30 14:25:08 -0700

    new

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,13 +6,22 @@
  * @n: size of second string
  * Return: Always 0.
  */
-char *_strncpy(char *dest, char *src, int n)
+char *rot13(char *S)
 {
-	int i;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i,J;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		*(dest + i) = *(src + i);
-	for ( ; i < n; i++)
-		*(dest + i) = '\0';
-	return (dest);
-}
+	for (i = 0; S[i] != '\0'; i++)
+	{
+		for(j=0;a[j]!='\0';j++)
+		{
+			if(s[i]==a[j])
+			{
+				s[i]=b[j];
+				break;
+			}
+		}
+	}
+   return s;	
+} 
