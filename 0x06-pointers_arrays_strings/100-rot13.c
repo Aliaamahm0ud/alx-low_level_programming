c54e9e929b16c76b293c88a87b4b2eb143061863
Aliaamahm0ud <loloeltorky165@gmail.com>
2023-03-30 15:20:03 -0700

    new

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,29 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _strncpy - concatenate strings, defining the size of second string.
- * @dest: string with concatenation
- * @src: string to be concatenated
- * @n: size of second string
- * Return: Always 0.
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
  */
-char *rot13(char *S)
+char *rot13(char *s)
 {
+	int i, j;
+
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	int i,J;
 
-	for (i = 0; S[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for(j=0;a[j]!='\0';j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if(s[i]==a[j])
+			if (s[i] == a[j])
 			{
-				s[i]=b[j];
+				s[i] = b[j];
 				break;
 			}
 		}
 	}
-   return s;	
+
+	return (s);
 } 
