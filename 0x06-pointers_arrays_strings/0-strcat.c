0ebea16da25a50f48d19fa6cda1f3d2c47cfc616
Aliaamahm0ud <loloeltorky165@gmail.com>
2023-03-29 23:09:09 -0700

    new task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,18 @@
-#include "main.h" 
+#include "main.h"
 /**
  * _strcat - concatenates 2 strings.
- * @dest: string with concatenation 
- * @src: string to be concatenated 
- * Return: Always 0. 
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
-{	
-	int lengthD, lengthS; 	
-	
-	
+{
+	int lengthD, lengthS;
 	lengthD = 0;
 	lengthS = 0;
 	while (*(dest + lengthD) != '\0')
 		lengthD++;
+
 	while (*(src + lengthS) != '\0' && lengthD < 97)
 	{
 		*(dest + lengthD) = *(src + lengthS);
