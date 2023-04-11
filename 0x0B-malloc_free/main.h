c40ac8c81ffde3f8c24e8a62ba5f30d288a1eb4e
Aliaamahm0ud <loloeltorky165@gmail.com>
2023-04-11 14:56:28 -0700

    new

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,16 +1,21 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#define MAIN_H
+
+
+char *create_array(unsigned int size, char c);
 
 int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
 
-#endif /* MAIN_H */
+char *_strdup(char *str);
+
+char *str_concat(char *s1, char *s2);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
+
+#endif
